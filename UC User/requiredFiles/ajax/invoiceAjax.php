1fc883fdd9d20830e58322f5c59f2249afcd12ec
gokul278 <gokulhk278@gmail.com>
2024-04-26 21:47:00 +0530

    Updated at 26/04/2024

--- UC User/requiredFiles/ajax/invoiceAjax.php
+++ UC User/requiredFiles/ajax/invoiceAjax.php
@@ -17,6 +17,9 @@ if ($way == "invoiceprint") {
 
     $pdf = new TCPDF();
 
+    $pdf->SetMargins(0, 0, 0);
+    $pdf->SetAutoPageBreak(false); // Disable auto page breaks
+
     $pdf->SetTitle('UCCASH TOURIST SAVINGS INVOICE');
 
     $pdf->SetPrintHeader(false);
@@ -24,25 +27,102 @@ if ($way == "invoiceprint") {
 
     $pdf->AddPage();
 
-    $pdf->SetFont('helvetica', '', 16);
-
-    // Set cell border to 0
-    $pdf->SetCellPadding(0);
-    $pdf->SetLineWidth(0);
-
-    $html = '
-    <div style="width:100%">
-    <img src="./img/InvoiceTop.jpg">
-    </div>
-    <table align="center" cellpadding="5"> 
+    $html = '    
+    <table align="center" style="margin: 0; padding: 0;"> 
+        <tr>
+            <td><img src="./img/InvoiceTop.jpg"/></td>
+        </tr>
+        <tr>
+            <td style="width:40%"></td>
+            <td style="font-size:25px;color:#6F6F6F;width:60%;" align="start"><b>Bill To:</b></td>
+        </tr>
+        <tr>
+            <td style="width:36%"></td>
+            <td style="width:60%;" align="start">
+                <div style="font-size:15.4px;color:#6F6F6F;border: 2px solid #F5BF26;width:100%; padding: 0; margin: 0;">
+                    <table align="center" style="width:100%; margin: 0;"> 
+                        <tr><td></td></tr>
+                        <tr>
+                            <td style="width:2%"></td>
+                            <td style="width:30%" align="start"><b>User ID</b></td>
+                            <td style="width:68%" align="start"><b>: UCT1002</b></td>
+                        </tr>
+                        <tr>
+                            <td style="width:2%"></td>
+                            <td style="width:30%" align="start"><b>User Name</b></td>
+                            <td style="width:68%" align="start"><b>: Gokul Moorthy</b></td>
+                        </tr>
+                        <tr>
+                            <td style="width:2%"></td>
+                            <td style="width:30%" align="start"><b>Email ID</b></td>
+                            <td style="width:68%" align="start"><b>: gokulhk278@gmail.com</b></td>
+                        </tr>
+                        <tr>
+                            <td style="width:2%"></td>
+                            <td style="width:30%" align="start"><b>Contact No</b></td>
+                            <td style="width:68%" align="start"><b>: +919842653413</b></td>
+                        </tr>
+                    </table>
+                </div>
+            </td>
+        </tr>
         <tr>
             <td></td>
+        </tr>
+        <tr>
             <td></td>
         </tr>
+        <tr>
+            <td style="width:2%"></td>
+            <td style="width:98%;color:#6F6F6F;font-size:16px" align="start">Date: 26-04-2024</td>
+        </tr>
+        <tr>
+            <td></td>
+        </tr>
+        <tr>
+            <td style="width:100%;font-size:14px;">
+                <table style="width:99%">
+                        <tr align="center">
+                            <td style="background-color:#011B3B;color:white">Invoice No</td>
+                            <td style="background-color:#011B3B;color:white">Invoice Date</td>
+                            <td style="background-color:#011B3B;color:white">Paid Date</td>
+                            <td style="background-color:#011B3B;color:white">Status</td>
+                            <td style="background-color:#011B3B;color:white">Savings TP</td>
+                            <td style="background-color:#011B3B;color:white">Savings Amount</td>
+                        </tr>
+                        <tr align="center">
+                            <td style="color:#6F6F6F;border: 1px solid #6F6F6F;"><b>1</b></td>
+                            <td style="color:#6F6F6F;border: 1px solid #6F6F6F;"><b>24-04-2024</b></td>
+                            <td style="color:#6F6F6F;border: 1px solid #6F6F6F;"><b>24-04-2024</b></td>
+                            <td style="color:#6F6F6F;border: 1px solid #6F6F6F;"><b>Paid</b></td>
+                            <td style="color:#6F6F6F;border: 1px solid #6F6F6F;"><b>55 TP</b></td>
+                            <td style="color:#6F6F6F;border: 1px solid #6F6F6F;font-size:19px;"><b>50 $</b></td>
+                        </tr>
+                        <tr align="center">
+                            <td style="width:83.35%;color:#6F6F6F;border: 1px solid #6F6F6F;font-size:20px;">
+                                <table style="margin: 0; padding: 0;">
+                                    <tr>
+                                        <td style="width:65%"></td>
+                                        <td style="color:#6F6F6F;font-size:20px;"><b>Total:</b></td>
+                                    </tr>
+                                </table>
+                            </td>
+                            <td style="width:16.65%;color:#6F6F6F;border: 1px solid #6F6F6F;font-size:20px;"><b>50 $</b></td>
+                        </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+        </tr>
+        <tr>
+            <td><img src="./img/InvoiceBottom.jpg"></td>
+        </tr>
     </table>
-    <div style="width:100%">
-    <img src="./img/InvoiceBottom.jpg">
-    </div>
+    
     ';
 
     $pdf->writeHTML($html);
